2805094c4cfb85632e417cbe59085069074bc272
zkac216 <zkac216@live.rhul.ac.uk>
2024-03-21 23:51:30 +0000

    Added function generateWeights to ISD class to generate an array of weights for the Ideal Soliton Distribution, the array is the CDF of probabilities.

--- FYP/DegreeDistribution.cs
+++ FYP/DegreeDistribution.cs
@@ -9,20 +9,39 @@ namespace FYP
 {
     internal class DegreeDistribution
     {
-        //float[] probabilities;
         protected int degree;
-        public DegreeDistribution(int k) {
+        protected int N; //size of data
+        public DegreeDistribution(int k, int N) {
             this.degree = k;
+            this.N = N;
         }
     }
 
     internal class ISD : DegreeDistribution 
-    { 
-        //Ideal Solution Distribution
-        public ISD(int k) : base(k) 
+    {
+        public double[] weights;
+        //Ideal Soliton Distribution
+        public ISD(int k, int N) : base(k, N) 
         { 
-            this.degree = k; 
-        }    
+            this.degree = k;
+            this.weights = new double[N];
+        }
+
+        public void generateWeights()
+        {
+            weights[0] = 1.0 / N;
+
+            for (int i = 2; i < N; i++)
+            {
+                weights[i - 1] = weights[i - 2] + pdf(i);
+                Console.WriteLine(weights[i - 1]);
+            }
+        }
+
+        private double pdf(double x) 
+        {
+            return 1.0 / (x * (x - 1));
+        }
 
     }
 }
