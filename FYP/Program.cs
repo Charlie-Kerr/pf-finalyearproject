16b5c085ff8ae86f158dc080552ca936ea0a3f12
zkac216 <zkac216@live.rhul.ac.uk>
2024-04-10 15:28:16 +0100

    First commit back after illness, refactor and tidy up of code before merge.

--- FYP/Program.cs
+++ FYP/Program.cs
@@ -7,8 +7,6 @@ namespace FYP
 {
     internal class Program
     {
-        public const int degree = 5; // will be attribute of encoder class
-
         public static Encoder encoder = new Encoder();
         public static Decoder decoder = new Decoder();
         public static ISD isd; //consider making this an attribute of the encoder class?
@@ -18,7 +16,6 @@ namespace FYP
             isd = new ISD(longerPlain.Length);//intiate ISD with size of data
 
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            //List<Drop> drops = generateDroplets(Encoding.ASCII.GetBytes(longerPlain));
             List<Drop> drops = ISDGenerateDroplets(Encoding.ASCII.GetBytes(longerPlain), longerPlain.Length);
             watch.Stop();
             var generateTime = watch.ElapsedMilliseconds;
@@ -26,7 +23,6 @@ namespace FYP
 
             watch.Restart();
             //test decode and rebuilding plaintext functions by printing decoded text to console
-            //Console.WriteLine(rebuildPlaintext(drops, Encoding.ASCII.GetByteCount(longerPlain)));
             Console.WriteLine(ISDRebuildPlaintext(drops, Encoding.ASCII.GetByteCount(longerPlain)));
             watch.Stop();
             var totalDecodetime = watch.ElapsedMilliseconds;
@@ -95,70 +91,6 @@ namespace FYP
             //returns the decoded data in a string format when every byte has been decoded
             return Encoding.ASCII.GetString(decoded);
         }
-        static string rebuildPlaintext(List<Drop> goblet, int byteSize) 
-        {
-            List<Drop> decodeBucket = new List<Drop>();
-            byte[] decoded = new byte[byteSize];
-            List<int> parts = new List<int>();
-            int dCount = 0;
-            int dPosition = 0;
-            bool allSolutionsFound = false;
-            byte nullValue = 0;
-
-            while (allSolutionsFound == false)
-            {
-                foreach (Drop drop in goblet)
-                {
-                    dCount = 0;
-                    if (drop.parts.Length == 1)
-                    {
-                        if (decoded[drop.parts[0]] == 0)
-                        {
-                            decoded[drop.parts[0]] = drop.data[0];
-                            parts.Add(drop.parts[0]);
-                            Console.WriteLine("Part " + drop.parts[0] + " has been decoded: [" + parts.Count + "/" + byteSize + " ]");
-                        }
-                        //else we discard the drop from the goblet, we already have a solution for it
-                    }
-                    else if (drop.parts.ToHashSet().IsSubsetOf(parts.ToHashSet()))
-                    {
-                        //else we discard the drop from the goblet, we already have the solutions for all the parts that are in the drop
-                    }
-                    else
-                    {
-                        //for drops degree > 1
-                        for (int i = 0; i < drop.parts.Length; i++)
-                        {
-                            if (parts.Contains(drop.parts[i]))
-                            {
-                                dCount++;
-                            }
-                            else
-                            {
-                                dPosition = i;
-                            }
-                        }
-
-                        if (dCount == drop.parts.Length - 1)
-                        {
-                            decoded[drop.parts[dPosition]] = decoder.decode(drop, decoded, dPosition); //consider parsing just the required bytes to decode the drop
-                            parts.Add(drop.parts[dPosition]);
-                            Console.WriteLine("Part " + drop.parts[dPosition] + " has been decoded: [" + parts.Count + "/" + byteSize + " ]");
-                        }
-                    }
-
-                    //checks if we have decoded the data
-                    if (!decoded.Contains(nullValue))
-                    { 
-                        allSolutionsFound = true;
-                        break;
-                    }
-
-                }
-            }
-            //returns the decoded data in a string format when every byte has been decoded
-            return Encoding.ASCII.GetString(decoded);
-        }
 
         static List<Drop> ISDGenerateDroplets(byte[] plain, int size)
         {
@@ -190,65 +122,6 @@ namespace FYP
                 drops.Add(new Drop(parts, encoder.encode(data, parts)));
             }
             return drops;
-        }
-
-        static List<Drop> generateDroplets(byte[] plain) 
-        {
-            Random rand = new Random();
-            int randPart = 0;
-            byte[] data = new byte[degree];
-            int dropletDegree;
-            int[] parts;
-            List<Drop> drops = new List<Drop>();
-
-
-            //this first loop is arbitrary and will be replaced by broadcasting method
-            for (int i = 0; i < plain.Length * 5; i++)
-            {
-                dropletDegree = getDegree();
-                parts = new int[dropletDegree];
-                //first drop should be degree 1 to make sure decoding process can happen
-                if (i == 0)
-                {
-                    dropletDegree = 1;
-                }
-
-                //second loop is where droplet generation happens
-                for (int j = 0; j < dropletDegree; j++)
-                {
-                    randPart = rand.Next(0, plain.Length);
-                    if (j == 0)
-                    {
-                        data[0] = plain[randPart];
-                        parts[0] = randPart;
-                    }
-                    else
-                    {
-                        data[j] = plain[randPart];
-                        parts[j] = randPart;
-                    }
-                }
-                drops.Add(new Drop(parts, encoder.encode(data, parts)));
-
-            }
-            return drops;
-        }
-
-        static int getDegree() {
-            int[] probabilities = { 50, 30, 15, 5, 1 };
-            Random rand = new Random();
-
-            int degree = rand.Next(1, 101);
-
-            foreach (int p in probabilities)
-            {
-                if (degree >= p)
-                {
-                    degree = Array.IndexOf(probabilities, p) + 1;
-                    return degree;
-                }
-            }
-            return -1; // a run should never reach here
         }
     }
 }
